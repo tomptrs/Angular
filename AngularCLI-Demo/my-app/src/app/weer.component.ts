6e18afba695b45817a804a39c476260bebdd5fb0
Tom Peeters <tom_ptrs@yahoo.com>
2017-09-05 14:11:11 +0200

    material design

--- AngularCLI-Demo/my-app/src/app/weer.component.ts
+++ AngularCLI-Demo/my-app/src/app/weer.component.ts
@@ -10,16 +10,22 @@ import {
 
 @Component ({ 
    selector: 'weer', 
-   template: '<div><button (click) = "sync()">Call Sync</button><button (click) = "async()">Call Async</button></div>', 
+   template: '<div><button (click) = "sync()">Call Sync</button><button (click) = "async()">Call Async</button><button (click) = "async2()">Call Async with Subject</button></div>', 
    providers: [WeerService] 
 }) 
 
 export class WeerComponent { 
   subscription: Subscription
-   
+   message:any;
     constructor(private weerService:WeerService) {
+       //?? this.weerService.Register().subscribe(message => { this.message = message;console.log(this.message);});
+        //.().subscribe(message => { this.message = message;
 
-        
+            this.weerService.Register().subscribe(message => { 
+                this.message = message;
+                console.log("in callback van observer")
+                console.log(this.message);
+            });
     }  
 
     sync(arg:any){
@@ -36,6 +42,10 @@ export class WeerComponent {
         //Call sync:
         let wb:WeerBericht;
         this.weerService.getWeerberichtVandaagAsync().then(data => { wb = data; console.log(wb);});    
+    }
+
+    async2(arg:any){
+        this.weerService.getWeerberichtVandaagAsync2();
     }
   
 
--- AngularCLI-Demo/my-app/src/app/weer.service.ts
+++ AngularCLI-Demo/my-app/src/app/weer.service.ts
@@ -3,21 +3,36 @@ import {
 } from '@angular/core';  
 import { HttpClient } from '@angular/common/http';
 import {WeerBericht} from './WeerBericht';
+import { Observable } from 'rxjs';
 import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class WeerService {  
 
-    
+    private subject = new Subject<any>();    
 
     constructor(private http: HttpClient){
        
     }
 
-    private subject = new Subject<any>();
+    Register(): Observable<any> {
+        console.log("register");
+        return this.subject.asObservable();
+    }
+
 
     sendMessage(message: any) {
         this.subject.next(message);//{ text: message });
+    }
+
+
+
+    getWeerBerichtSync(){
+        let wb:WeerBericht = new WeerBericht();
+        wb.Beschrijving = "testje";
+        wb.Temperatuur = 22;
+
+        return wb;
     }
    
    getWeerberichtVandaag(){
@@ -47,6 +62,7 @@ getHeroesSlowly(): Promise<Hero[]> {
        wb.Beschrijving = "testje";
        wb.Temperatuur = 22;
        //return Promise.resolve(wb);
+      
 
        return new Promise(resolve => {
            setTimeout(function() {
@@ -54,5 +70,20 @@ getHeroesSlowly(): Promise<Hero[]> {
            }, 2000);
        })
          
-   } 
+   }
+    
+     getWeerberichtVandaagAsync2(){
+       //src="./assets/logo.png">
+       let wb:WeerBericht = new WeerBericht();
+       wb.Beschrijving = "blabla";
+       wb.Temperatuur = 29;
+       //return Promise.resolve(wb);
+
+       
+       //this.subject.next(wb);
+        this.subject.next(wb);
+           
+       
+         
+   }
 }
